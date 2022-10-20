9853f9c2039122782fb8c5274d3e79ad80112e89
Vincent <vincent@rockx.com>
2022-10-20 11:41:09 +0800

    feat: style

--- src/WalletCreator.tsx
+++ src/WalletCreator.tsx
@@ -36,7 +36,7 @@ const WalletRenderer = ({ wallet }: { wallet: Wallet }) => {
     return [containChars.extractRoot(), containChars.extractRoot()];
   }, [wallet]);
   return (
-    <details className="px-2 py-1 even:bg-gray-50 odd:bg-white overflow-auto">
+    <details className="px-2 py-1 even:bg-gray-50 odd:bg-white">
       <summary className="font-mono text-sm">
         {wallet.address.split('').map((v: string, i: number) => {
           const colors = ['text-green-600', 'text-yellow-600'];
@@ -60,9 +60,10 @@ const WalletRenderer = ({ wallet }: { wallet: Wallet }) => {
           className={classNames('ml-1', {
             'text-red-100': containChars[0].times < 8,
             'bg-red-200 text-white': containChars[0].times === 8,
-            'bg-red-300 text-white': containChars[0].times === 9,
-            'bg-red-500 text-white': containChars[0].times === 10,
-            'bg-red-700 text-white': containChars[0].times >= 11,
+            'bg-red-400 text-white': containChars[0].times === 9,
+            'bg-red-600 text-white': containChars[0].times === 10,
+            'bg-red-900 text-white': containChars[0].times === 11,
+            'bg-black text-white': containChars[0].times >= 12,
           })}
         >
           {containChars[0].times}
@@ -74,52 +75,58 @@ const WalletRenderer = ({ wallet }: { wallet: Wallet }) => {
     </details>
   );
 };
-
 export default function WalletCreator() {
   const [wallets, setWallets] = useState<Wallet[]>([]);
-  const [isRunning, setIsRunning] = useState(true);
-  const isRunningRef = useRef(true);
-  const shouldClearRef = useRef(false);
-  isRunningRef.current = isRunning;
+  // const [isRunning, setIsRunning] = useState(true);
+  const isRunningRef = useRef<HTMLInputElement>(null);
+
+  const animationFrameRef = useRef<number>();
+
   useEffect(() => {
-    const createWallet = async () => {
-      if (isRunningRef.current) {
-        await new Promise(resolve => setTimeout(resolve));
+    const onAnimationFrame = () => {
+      if (isRunningRef.current?.checked) {
         const wallet = ethers.Wallet.createRandom(); // 随机生成钱包，安全
-        const shouldClear = shouldClearRef.current;
-        shouldClearRef.current = false;
-        setWallets(prevState =>
-          shouldClear ? [wallet] : [...prevState, wallet]
-        );
+        setWallets(prevState => [...prevState, wallet]);
       }
+      animationFrameRef.current = requestAnimationFrame(onAnimationFrame);
     };
-    createWallet();
+    animationFrameRef.current = requestAnimationFrame(onAnimationFrame);
+    return () => {
+      cancelAnimationFrame(animationFrameRef.current!);
+    };
   });
+
   const onClearClick = useCallback(() => {
-    alert(123);
-    shouldClearRef.current = true;
+    setWallets([]);
   }, []);
 
   return (
-    <div className="w-full h-screen space-y-2 overflow-auto">
-      {shouldClearRef.current ? 1 : 0}
-      <div className="p-2 flex space-x-2 sticky top-0 bg-gray-50 z-10">
-        <button
+    <div className="w-full h-screen flex flex-col gap-2">
+      <div className="p-2 border-b border-gray-100 flex-shrink-0 flex space-x-2 sticky top-0 bg-gray-50 z-10">
+        <label className="inline-flex items-center">
+          <input
+            type="checkbox"
+            className="form-checkbox rounded text-pink-500"
+            ref={isRunningRef}
+          />
+          <span className="ml-2">Running</span>
+        </label>
+        {/* <button
           type="button"
           onClick={() => setIsRunning(v => !v)}
           className="py-2 px-3 text-sm font-medium text-gray-500 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-50 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-600 select-none"
         >
           {isRunning ? 'Pause' : 'Resume'}
-        </button>
+        </button> */}
         <button
           type="button"
           onClick={onClearClick}
           className="py-2 px-3 text-sm font-medium text-gray-500 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-50 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-600 select-none"
         >
-          clear
+          Clear
         </button>
       </div>
-      <div className="h-full">
+      <div className="p-2 flex-auto overflow-auto grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
         {wallets.map((wallet, i) => (
           <WalletRenderer wallet={wallet} key={i} />
         ))}
